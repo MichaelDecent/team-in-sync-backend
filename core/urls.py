abdb5f42098cb5c194b86e7e100d49dd0e4b972c
MichaelDecent <michaeldecent2@gmail.com>
2025-03-23 18:46:54 +0100

    Fix static file serving in URL configuration

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
-import static
 
 API_V1 = "api/v1/"
 
