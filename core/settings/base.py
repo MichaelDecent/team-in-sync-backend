941faf78f4cb978a45571bf938d07a2e3e100426
MichaelDecent <michaeldecent2@gmail.com>
2025-03-23 18:53:20 +0100

    Fix typo in STATICFILES_STORAGE setting in base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -106,7 +106,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'fr
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
